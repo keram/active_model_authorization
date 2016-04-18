62f85c6e824917be5a6be804035c0d584f8d6850
Marek L <nospam.keram@gmail.com>
2016-04-18 11:02:03 +0100

    Move preload to `Concern.included` stage as that is the latest stage
    where we can assume that $LOAD_PATH includes all load paths for
    authorizations.
    For example in rails `app/models/` when we store `authorizations` in
    `app/models/authorizations/`

--- lib/active_model_authorization/authorization_file_preloader.rb
+++ lib/active_model_authorization/authorization_file_preloader.rb
@@ -1,19 +1,24 @@
 # frozen_string_literal: true
 module ActiveModelAuthorization
   module AuthorizationFilePreloader
-    $LOAD_PATH
-      .select do |path|
-        Dir.exist? File.join(path,
-                             'authorizations')
-      end
-      .map do |path|
-        File.join(path,
-                  'authorizations',
-                  '**',
-                  '*authorization.rb')
-      end
-      .each do |path|
-        Dir.glob(path).each { |file| require(file) }
-      end
+    def self.preload
+      $LOAD_PATH
+        .select do |path|
+          Dir.exist? File.join(path,
+                               'authorizations')
+        end
+        .map do |path|
+          File.join(path,
+                    'authorizations',
+                    '**',
+                    '*authorization.rb')
+        end
+        .flat_map do |path|
+          Dir.glob(path)
+        end
+        .each do |file|
+          require(file)
+        end
+    end
   end
 end
--- lib/active_model_authorization/concern.rb
+++ lib/active_model_authorization/concern.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 require 'active_model_authorization/concern/class_methods'
 require 'active_model_authorization/concern/instance_methods'
+require 'active_model_authorization/authorization_file_preloader'
 
 module ActiveModelAuthorization
   module Concern
     def self.included(klass)
       klass.extend ClassMethods
       klass.include InstanceMethods
+      AuthorizationFilePreloader.preload
     end
   end
 end
